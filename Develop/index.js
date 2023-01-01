5076863c33f313f402bb264a4e63a681ac6274a9
Kyle Franklin <kpfranklin22@gmail.com>
2023-01-01 16:07:56 -0500

    working on function init and function generateMarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,73 +1,75 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+const markdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
-{
-    type:"input",
-    name:"title",
-    message:"Project title?",
-},
-{
-    type:"input",
-    name:"description",
-    message:"Project description?",
-},
-{
-    type:"input",
-    name:"installation",
-    message:"Installation instructions?",
-},
-{
-    type:"input",
-    name:"usage",
-    message:"Project usage information?",
-},
-{
-    type:"input",
-    name:"contribution",
-    message:"Contribution information?",
-},
-{
-    type:"input",
-    name:"tests",
-    message:"Test instructions?",
-},
-{
-    type:"input",
-    name:"email",
-    message:"For questions(email)?",
-},
-{
-    type:"input",
-    name:"github",
-    message:"For questions(github)?",
-},
-{
-    type:"list",
-    name:"license",
-    message:"License?",
-    choices:["MIT","ISC","Mozilla Public License 2.0","Apache License 2.0"],
-    filter(value){
-        return value.toLowerCase();
-    }
-},
+  {
+    type: "input",
+    name: "title",
+    message: "Project title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Project description?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Installation instructions?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Project usage information?",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Contribution information?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Test instructions?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "For questions(email)?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "For questions(github)?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "License?",
+    choices: ["MIT", "ISC", "Mozilla Public License 2.0", "Apache License 2.0"],
+    filter(value) {
+      return value.toLowerCase();
+    },
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {};
+function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    return inquirer.prompt(questions)
-    .then((data)=> {
-        console.log(data)
-        return data
+  return inquirer
+    .prompt(questions)
+    .then((data) => {
+      const mark = markdown.generateMarkdown(data);
+      console.log(mark);
+      return data;
     })
-    .catch((error)=> {
-        console.log(error)
-    })
-
-};
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -11,8 +11,39 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
+  console.log(data);
+
   return `# ${data.title}
+
+  ## Table of Contents
+  - [Project Description](#Description)
+  - [Usage](#Usage)
+  - [Contributions](#Contributions)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+  - [License](#License)
+
+  ## Description
+  ${data.description}
+
+  ## Usage
+  This application is used for: ${data.usage}
+
+  ## Contributions
+  Contributors: ${data.contributions}
+
+  ## Tests
+  To run tests, run the following command(s): ${data.tests}
+
+  ## Questions
+  If you have any questions about the repository, you can contact me here:
+  Email: ${data.email}
+  Github: ${data.github}
+
+  ## License
+  ${data.license}
 `;
 }
 
