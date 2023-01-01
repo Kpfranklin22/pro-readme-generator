854eeb7e26069ae2db417f3b1f6088c8e6042d64
Kyle Franklin <kpfranklin22@gmail.com>
2023-01-01 14:50:29 -0500

    added questions array and function init

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,73 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type:"input",
+    name:"title",
+    message:"Project title?",
+},
+{
+    type:"input",
+    name:"description",
+    message:"Project description?",
+},
+{
+    type:"input",
+    name:"installation",
+    message:"Installation instructions?",
+},
+{
+    type:"input",
+    name:"usage",
+    message:"Project usage information?",
+},
+{
+    type:"input",
+    name:"contribution",
+    message:"Contribution information?",
+},
+{
+    type:"input",
+    name:"tests",
+    message:"Test instructions?",
+},
+{
+    type:"input",
+    name:"email",
+    message:"For questions(email)?",
+},
+{
+    type:"input",
+    name:"github",
+    message:"For questions(github)?",
+},
+{
+    type:"list",
+    name:"license",
+    message:"License?",
+    choices:["MIT","ISC","Mozilla Public License 2.0","Apache License 2.0"],
+    filter(value){
+        return value.toLowerCase();
+    }
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+    .then((data)=> {
+        console.log(data)
+        return data
+    })
+    .catch((error)=> {
+        console.log(error)
+    })
+
+};
 
 // Function call to initialize app
 init();
